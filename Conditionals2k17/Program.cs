0e76676bd436f60a438302c139fa7ea5e0f11c94
Frank Nasseri <franknasseri57@gmail.com>
2017-06-28 14:22:55 -0400

    Agedistrubtion

--- Conditionals2k17/Program.cs
+++ Conditionals2k17/Program.cs
@@ -34,41 +34,86 @@ namespace Conditionals2k17
             //Console.WriteLine("What is the the name on the reservation?");
             //string reserveName = Console.ReadLine();
 
-            Console.WriteLine("Thank you for eating at Frankie's Place!");
-            Console.WriteLine("What is your total bill?");
+            //Console.WriteLine("Thank you for eating at Frankie's Place!");
+            //Console.WriteLine("What is your total bill?");
 
-            double billTotal = double.Parse(Console.ReadLine());
-            double discount10 = billTotal - (billTotal * .10d);
-            double discount5 = billTotal - (billTotal * .05d);
-            double billSplit;
+            //double billTotal = double.Parse(Console.ReadLine());
+            //double discount10 = billTotal - (billTotal * .10d);
+            //double discount5 = billTotal - (billTotal * .05d);
+            //double billSplit;
 
-            Console.WriteLine("How many people in your party?");
-            int numberOfPeople = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How many people in your party?");
+            //int numberOfPeople = int.Parse(Console.ReadLine());
 
-            if (billTotal >= 50)
+            //if (billTotal >= 50)
+            //{
+            //    Console.WriteLine("You get 10% off of your bill!");
+            //    Console.WriteLine("Your total with your discount: $" + discount10);
+            //    billSplit = discount10 / numberOfPeople;
+            //    Console.WriteLine("Each person in your party will pay");
+            //    Console.WriteLine("Each person in your party will pay: $" + billSplit);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You get 5% off of you bill!");
+            //    Console.WriteLine("Your total with your discount: $" + discount5);
+            //    billSplit = discount5 / numberOfPeople;
+            //    Console.WriteLine("Each person in your party will pay: $" + billSplit);
+            //}
+            ////how mucheach person is paying?
+
+
+            Console.WriteLine("How old is your child?");
+            int childAge = int.Parse(Console.ReadLine());
+
+            if (childAge <= 2)
             {
-                Console.WriteLine("You get 10% off of your bill!");
-                Console.WriteLine("Your total with your discount: $" + discount10);
-                billSplit = discount10 / numberOfPeople;
-                Console.WriteLine("Each person in your party will pay");
-                Console.WriteLine("Each person in your party will pay: $" + billSplit);
+                Console.WriteLine("Still in Mama's Arms");
+            }
+
+            else if (childAge <= 5)
+            {
+                Console.WriteLine("Preschool Maniac");
+            }
+            else if (childAge <=11)
+            {
+                Console.WriteLine("Elementary School");
+            }
+            else if (childAge <= 14)
+            {
+                Console.WriteLine("Middle School");
+            }
+            else if (childAge <= 18)
+            {
+                Console.WriteLine("High-School");
+            }
+            else if (childAge <= 22)
+            {
+                Console.WriteLine("College");
+            }
+            else if (childAge <= 65)
+            {
+                Console.WriteLine("Working for the Man");
+            }
+            else if (childAge <= 100)
+            {
+                Console.WriteLine("The Golden Years");
             }
             else
             {
-                Console.WriteLine("You get 5% off of you bill!");
-                Console.WriteLine("Your total with your discount: $" + discount5);
-                billSplit = discount5 / numberOfPeople;
-                Console.WriteLine("Each person in your party will pay: $" + billSplit);
+                Console.WriteLine("This Program is For Humans");
+
             }
-            //how mucheach person is paying?
-           
 
-            
-            
 
-            
 
-        
+
+
+
+
+
+
+
         }
     }
 }
